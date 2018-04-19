afc8c9065fc45e0b5d39f5e48f0d14f81f3ccbb9
Joshua Burgin <jsburgin@crimson.ua.edu>
2018-04-19 16:23:11 -0500

    Clean up formatting

--- task4a-songs.php
+++ task4a-songs.php
@@ -1,45 +1,45 @@
-<?php 
-	$user = "jsburgin";
-	$cwid = "11484207";
-	$server = "cs-sql2014.ua-net.ua.edu";
-	// create the connection to mySQL
-	$con = new mysqli($server, $user, $cwid, $user);
-	
+<?php
+  $user = "jsburgin";
+  $cwid = "11484207";
+  $server = "cs-sql2014.ua-net.ua.edu";
+  // create the connection to mySQL
+  $con = new mysqli($server, $user, $cwid, $user);
 
-	$columns = $con->query("SHOW COLUMNS FROM song");	
-	$rows = $con->query("SELECT * FROM song WHERE genre = '" . $_POST['genre'] . "'");
+
+  $columns = $con->query("SHOW COLUMNS FROM song");
+  $rows = $con->query("SELECT * FROM song WHERE genre = '" . $_POST['genre'] . "'");
 ?>
 
 <!doctype html>
 
 <html>
 <head>
-	<title>Task 4a - Songs</title>
-	<link href="/~jsburgin/style.css" rel="stylesheet" />
+  <title>Task 4a - Songs</title>
+  <link href="/~jsburgin/style.css" rel="stylesheet" />
 </head>
-	
+
 <body>
-	<a href="/~jsburgin" class="return-link">Return to Main Page</a>		
-	<div class="section">
-		<h3>Showing All <?php echo $_POST['genre'] ?> Songs</h3>
-		<table>
-			<thead>
-				<tr>
-					<?php while ($column = mysqli_fetch_row($columns)) { ?>
-						<th><?php echo $column[0]; ?></th>
-					<?php } ?>
-				</tr>
-			</thead>
-			<tbody>
-				<?php while ($row = mysqli_fetch_row($rows)) { ?>
-					<tr>
-						<?php foreach($row as $value) {?>
-							<td><?php echo $value ?></td>
-						<?php } ?>
-					</tr>
-				<?php } ?>
-			</tbody>
-		</table>
-	</div>
+  <a href="/~jsburgin" class="return-link">Return to Main Page</a>
+  <div class="section">
+    <h3>Showing All <?php echo $_POST['genre'] ?> Songs</h3>
+    <table>
+      <thead>
+        <tr>
+          <?php while ($column = mysqli_fetch_row($columns)) { ?>
+            <th><?php echo $column[0]; ?></th>
+          <?php } ?>
+        </tr>
+      </thead>
+      <tbody>
+        <?php while ($row = mysqli_fetch_row($rows)) { ?>
+          <tr>
+            <?php foreach($row as $value) {?>
+              <td><?php echo $value ?></td>
+            <?php } ?>
+          </tr>
+        <?php } ?>
+      </tbody>
+    </table>
+  </div>
 </body>
 </html>
--- task4b-search.php
+++ task4b-search.php
@@ -1,47 +1,47 @@
-<?php 
-	$user = "jsburgin";
-	$cwid = "11484207";
-	$server = "cs-sql2014.ua-net.ua.edu";
-	// create the connection to mySQL
-	$con = new mysqli($server, $user, $cwid, $user);
-	
-	$name = $_POST["name"];
+<?php
+  $user = "jsburgin";
+  $cwid = "11484207";
+  $server = "cs-sql2014.ua-net.ua.edu";
+  // create the connection to mySQL
+  $con = new mysqli($server, $user, $cwid, $user);
 
-	$columns = $con->query("SHOW COLUMNS FROM employee");	
-	$rows = $con->query("SELECT * FROM employee WHERE name LIKE '%$name%'");	
+  $name = $_POST["name"];
+
+  $columns = $con->query("SHOW COLUMNS FROM employee");
+  $rows = $con->query("SELECT * FROM employee WHERE name LIKE '%$name%'");
 ?>
 
 <!doctype html>
 
 <html>
 <head>
-	<title>Task 4a - Songs</title>
-	<link href="/~jsburgin/style.css" rel="stylesheet" />
+  <title>Task 4a - Songs</title>
+  <link href="/~jsburgin/style.css" rel="stylesheet" />
 </head>
-	
+
 <body>
-	<a href="/~jsburgin" class="return-link">Return to Main Page</a>		
-	
-	<div class="section">
-		<h3>Searchings Employees With Name "<?php echo $name ?>"</h3>
-		<table>
-			<thead>
-				<tr>
-					<?php while ($column = mysqli_fetch_row($columns)) { ?>
-						<th><?php echo $column[0]; ?></th>
-					<?php } ?>
-				</tr>
-			</thead>
-			<tbody>
-				<?php while ($row = mysqli_fetch_row($rows)) { ?>
-					<tr>
-						<?php foreach($row as $value) {?>
-							<td><?php echo $value ?></td>
-						<?php } ?>
-					</tr>
-				<?php } ?>
-			</tbody>
-		</table>
-	</div>
+  <a href="/~jsburgin" class="return-link">Return to Main Page</a>
+
+  <div class="section">
+    <h3>Searchings Employees With Name "<?php echo $name ?>"</h3>
+    <table>
+      <thead>
+        <tr>
+          <?php while ($column = mysqli_fetch_row($columns)) { ?>
+            <th><?php echo $column[0]; ?></th>
+          <?php } ?>
+        </tr>
+      </thead>
+      <tbody>
+        <?php while ($row = mysqli_fetch_row($rows)) { ?>
+          <tr>
+            <?php foreach($row as $value) {?>
+              <td><?php echo $value ?></td>
+            <?php } ?>
+          </tr>
+        <?php } ?>
+      </tbody>
+    </table>
+  </div>
 </body>
 </html>
